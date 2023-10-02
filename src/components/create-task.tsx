5ca72e6eab17bd2fe6441bc2acc32b4e95bf6d17
Mathew <mathewjohncorreia@gmail.com>
2023-10-02 17:01:41 +0100

    feat: add `createTask` function

--- src/components/create-task.tsx
+++ src/components/create-task.tsx
@@ -1,22 +1,58 @@
+"use client";
+import { useState, useRef } from "react";
 import { PlusIcon } from "lucide-react";
 
 export function CreateTask() {
+  const taskNameRef = useRef<HTMLInputElement | null>(null);
+  const taskPointsRef = useRef<HTMLInputElement | null>(null);
+
+  function createTask() {
+    if (!taskNameRef.current || !taskPointsRef.current) {
+      alert("error: inputs dont exist");
+      return;
+    }
+
+    const name = taskNameRef.current.value;
+    const points = taskPointsRef.current.valueAsNumber;
+
+    if (name === "") {
+      alert("No name");
+      return;
+    }
+
+    if (Number.isNaN(points)) {
+      alert("No points");
+      return;
+    }
+
+    //post task database
+    taskNameRef.current.value = "";
+    taskPointsRef.current.value = "";
+  }
+
   return (
     <div className="flex flex-row gap-2 items-end">
       <div className="w-2/3">
         <label className="">Task Name</label>
-        <input className="border rounded-md shadow py-1 px-2 text-xl" />
+        <input
+          ref={taskNameRef}
+          className="border rounded-md shadow py-1 px-2 text-xl"
+        />
       </div>
 
       <div className="w-full">
         <label className="">Points</label>
         <input
+          ref={taskPointsRef}
           className="border rounded-md shadow py-1 px-2 text-xl w-full"
           type="number"
         />
       </div>
 
-      <button className="border h-fit p-1 rounded-md shadow w-[36px]">
+      <button
+        onClick={createTask}
+        className="border h-fit p-1 rounded-md shadow w-[36px]"
+      >
         <PlusIcon size={28} />
       </button>
     </div>
