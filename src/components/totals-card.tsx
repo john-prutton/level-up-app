2c51812a71dd0873de19fe88d956da839ff9a221
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-02 12:29:16 +0200

    skeleton home page and total component

---
+++ src/components/totals-card.tsx
@@ -0,0 +1,13 @@
+export type TTotalsCard = {
+	points: number
+	label: string
+}
+
+export function TotalCard({ data: { label, points } }: { data: TTotalsCard }) {
+	return (
+		<div className="border w-full p-4 rounded-md shadow">
+			<p className="text-center font-bold">{points} pts</p>
+			<p className="text-center">{label}</p>
+		</div>
+	)
+}
