b3a375948298edb6d2e3bbf51571011631338112
Mathew <mathewjohncorreia@gmail.com>
2023-10-03 12:54:14 +0100

    feat: add totals functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,54 @@ import { CreateTask } from "@/components/create-task";
 import { getTasks } from "@/lib/api/tasks/queries";
 
 export default async function Home() {
-  const totals: TTotalsCard[] = [
-    {
-      points: 1550,
-      label: "today",
-    },
-    {
-      points: 25500,
-      label: "this week",
-    },
-    {
-      points: 2500,
-      label: "left to earn",
-    },
-  ];
-
   const { tasks } = await getTasks();
-  const todos = tasks.filter(function (t) {
+
+  const incompleteTasks = tasks.filter(function (t) {
     return t.status === "INCOMPLETE";
   });
+
+  const completeTasks = tasks.filter(function (t) {
+    return t.status === "COMPLETE";
+  });
+
+  const totals: TTotalsCard[] = [
+    {
+      label: "earned today",
+      value:
+        completeTasks
+          .filter((t) => {
+            const yesterday = Date.now() - 24 * 60 * 60 * 1000;
+
+            const d = t.completedAt!.getTime();
+            console.log(d);
+
+            return d >= yesterday;
+          })
+          .reduce((totalsTask, task) => ({
+            ...totalsTask,
+            points: totalsTask.points + task.points,
+          })).points + " pts",
+    },
+    {
+      label: "tasks complete",
+      value: completeTasks.length + "",
+    },
+    {
+      label: "left to earn",
+      value:
+        incompleteTasks.length === 0
+          ? "0 pts"
+          : incompleteTasks.reduce((totalsTask, task) => ({
+              ...totalsTask,
+              points: totalsTask.points + task.points,
+            })).points + " pts",
+    },
+  ];
 
   return (
     <div>
       <h2 className="text-3xl mb-4">Totals</h2>
-      <div className="flex flex-row justify-between gap-2">
+      <div className="flex flex-col md:flex-row justify-between gap-2">
         {totals.map((element, i) => (
           <TotalCard key={i} data={element} />
         ))}
@@ -38,7 +62,7 @@ export default async function Home() {
 
       <h2 className="text-3xl my-4">Tasks</h2>
       <div className="flex flex-col justify-between gap-2">
-        {todos.map((task, i) => (
+        {incompleteTasks.map((task, i) => (
           <TaskCard key={i} data={task} />
         ))}
       </div>
--- src/components/totals-card.tsx
+++ src/components/totals-card.tsx
@@ -1,12 +1,12 @@
 export type TTotalsCard = {
-  points: number;
   label: string;
+  value: string;
 };
 
-export function TotalCard({ data: { label, points } }: { data: TTotalsCard }) {
+export function TotalCard({ data: { label, value } }: { data: TTotalsCard }) {
   return (
-    <div className="border w-full p-4 rounded-md shadow">
-      <p className="text-center font-bold">{points} pts</p>
+    <div className="border w-full p-4 rounded-md shadow flex flex-col justify-center">
+      <p className="text-center font-bold">{value}</p>
       <p className="text-center">{label}</p>
     </div>
   );
